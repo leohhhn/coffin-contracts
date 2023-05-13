065a9a8a5e895117fb62195d752ca4046adc8f88
leon <leonhudak@protonmail.com>
2023-05-13 21:13:14 +0200

    createLeveragedPosition functions done

--- contracts/CoffinVault.sol
+++ contracts/CoffinVault.sol
@@ -6,8 +6,9 @@ pragma solidity ^0.8.9;
 
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
 import {DataTypes} from "@aave/core-v3/contracts/protocol/libraries/types/DataTypes.sol";
+import "@aave/core-v3/contracts/misc/interfaces/IWETH.sol";
 
-import "./../../node_modules/@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -36,7 +37,7 @@ contract CoffinVault is Ownable, ICoffinVault {
     }
 
     // user needs to approve spending token before calling this function
-    function createPosition(Position memory _position) external {
+    function createLeveragedPosition(Position memory _position) external {
         // assume only USDC
         require(
             _position.token != address(0),
@@ -51,42 +52,37 @@ contract CoffinVault is Ownable, ICoffinVault {
         // get reserve data for collateral, in our case weth
         address WETH = addressRegistry.getWETH();
         uint16 ltv = getLTVForAsset(WETH);
-        require(ltv >= 10, "CoffinVault:createPosition:: LTV too low");
-
-        uint32 maxMultiplier = 1000 + (ltv / 10);
+        require(ltv >= 100, "CoffinVault:createPosition:: Reserve LTV too low");
 
         // example math for frontend calculation of multiplier
         // ltv 7500 => 75% from aave
         // => max leverage = x 1.75 => 1750 bp
 
-        // LTV80 : 100 = max lev :
-
         // check if leverage provided is possible
         require(
-            _position.leverage <= maxMultiplier,
+            _position.leverage <= ltv,
             "CoffinVault:createPosition:: Leverage above LTV"
         );
 
+        address vault = address(this);
         address USDC = addressRegistry.getUSDC();
 
         ISwapRouter swapRouter = ISwapRouter(addressRegistry.getUniswap());
         TransferHelper.safeTransferFrom(
             USDC,
             msg.sender,
-            address(this),
+            vault,
             _position.amount
         );
 
         TransferHelper.safeApprove(USDC, address(swapRouter), _position.amount);
 
-        // swap all of single token for WETH
-
         ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
             .ExactInputSingleParams({
                 tokenIn: USDC,
                 tokenOut: WETH,
-                fee: poolFee,
-                recipient: msg.sender,
+                fee: poolFee, // assuming there exists a USDC/WETH 0.3% pool
+                recipient: vault,
                 deadline: block.timestamp,
                 amountIn: _position.amount,
                 amountOutMinimum: 0, // todo set in prod
@@ -99,35 +95,73 @@ contract CoffinVault is Ownable, ICoffinVault {
         // supply weth to aave
         aavePool.supply(WETH, wethAmount, msg.sender, 0);
 
-        (uint256 totalCollateralETH, , , , , , , , , ) = aavePool
-            .getUserAccountData(userAddress);
+        (uint256 totalCollateralETH, , , , , ) = aavePool.getUserAccountData(
+            vault // todo check this
+        );
 
-        // Calculate maximum borrow amount
-        uint256 maxBorrowAmountInWei = (totalCollateralETH * ltv) / 10000;
+        // Calculate borrow amount
+        uint256 maxBorrowAmountInWei = (totalCollateralETH *
+            _position.leverage) / 10000;
 
         address GHO = addressRegistry.getGHO();
 
-        aavePool.borrow(
-            GHO,
-            maxBorrowAmountInWei,
-            interestRateMode,
-            referralCode,
-            onBehalfOf
-        );
+        aavePool.borrow(GHO, maxBorrowAmountInWei, 1, 0, vault);
 
-        // supply weth
-        // borrow GHO against it
-        // swap 50% for weth
-        // LP into uni v3
-        //     struct Position {
-        //     address _token; // 0x0 if ETH
-        //     uint32 _leverage; // basis point leverage /// 2x => 2000 bp, 1x => 1000 bp
-        //     bool active;
-        //     uint256 _amount; // if eth, same amount as msg.value  supply through .call(""){}
-        // }
+        uint256 borrowedGHOAmount = IERC20(GHO).balanceOf(vault);
+
+        emit CreatedLeveragedPosition(vault, borrowedGHOAmount);
     }
 
-    function createPositionETH(Position memory _position) external payable {}
+    function createLeveragedPositionETH(
+        Position memory _position
+    ) external payable {
+        // doesnt check position.amount. just uses msg.value instead
+        require(
+            _position.token == address(0),
+            "CoffinVault:createPosition:: Token address must be 0x0"
+        );
+
+        require(
+            _position.leverage >= 100,
+            "CoffinVault:createPosition:: Leverage cannot be less than x1"
+        );
+
+        // get reserve data for collateral, in our case weth
+        address wethAddress = addressRegistry.getWETH();
+        uint16 ltv = getLTVForAsset(wethAddress);
+        require(ltv >= 100, "CoffinVault:createPosition:: Reserve LTV too low");
+
+        // check if leverage provided is possible
+        require(
+            _position.leverage <= ltv,
+            "CoffinVault:createPosition:: Leverage above LTV"
+        );
+
+        address vault = address(this);
+
+        IWETH(wethAddress).deposit{value: msg.value}();
+
+        IPool aavePool = IPool(addressRegistry.getAave());
+
+        // supply weth to aave
+        aavePool.supply(wethAddress, msg.value, msg.sender, 0);
+
+        (uint256 totalCollateralETH, , , , , ) = aavePool.getUserAccountData(
+            vault // todo check this
+        );
+
+        // Calculate borrow amount
+        uint256 maxBorrowAmountInWei = (totalCollateralETH *
+            _position.leverage) / 10000;
+
+        address GHO = addressRegistry.getGHO();
+
+        aavePool.borrow(GHO, maxBorrowAmountInWei, 1, 0, vault);
+
+        uint256 borrowedGHOAmount = IERC20(GHO).balanceOf(vault);
+
+        emit CreatedLeveragedPosition(vault, borrowedGHOAmount);
+    }
 
     function withdrawPosition() external {}
 
--- contracts/interfaces/ICoffinVault.sol
+++ contracts/interfaces/ICoffinVault.sol
@@ -2,18 +2,19 @@
 pragma solidity ^0.8.9;
 
 interface ICoffinVault {
+    event CreatedLeveragedPosition(address vault, uint256 indexed borrowedAmountOfGHO); // todo see what is needed on the front
+
     struct Position {
         address token; // 0x0 if ETH
         uint16 leverage; // passed in after getting ltv from getReserveData. ie ltv = 7500 (75%), max leverage = 7500
-       
         bool active;
         uint256 amount; // if eth, same amount as msg.value  supply through .call(""){}
     }
 
     // user uses only single token at beginning. ie USDC/GHO
-    function createPosition(Position memory _position) external;
+    function createLeveragedPosition(Position memory _position) external;
 
-    function createPositionETH(Position memory _position) external payable;
+    function createLeveragedPositionETH(Position memory _position) external payable;
 
     function withdrawPosition() external;
 
